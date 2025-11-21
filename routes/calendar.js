55a77a82e1a15db4ffead95bf6a503e85a4e6633
Lustiora <parkjaegun0219@gmail.com>
2025-11-21 16:12:31 +0900

    calender > calendar 오타 수정

--- routes/calendar.js
+++ routes/calendar.js
@@ -6,15 +6,15 @@ var router = express.Router() // 함수의 ()괄호가 아닌 객체의 ()괄호
 // 1. endpoint -> http://localhost:3000/calender
 // 2. 콜백함수 function(req[요청],res[응답],next[미들웨어 호출함수])
 router.get("/", function(req,res,next){
-  res.send('Calender Page List')
+  res.send('Calendar Page List')
 })
-router.get("/calender01", function(req,res,next){
-  res.render('index', {title : 'Annual Schedule', pageName :'pages/calender/calender01.ejs'})
+router.get("/calendar01", function(req,res,next){
+  res.render('index', {title : 'Annual Schedule', pageName :'pages/calendar/calendar01.ejs'})
 })
-router.get("/calender02", function(req,res,next){
-  res.render('index', {title : 'Monthly Schedule', pageName :'pages/calender/calender02.ejs'})
+router.get("/calendar02", function(req,res,next){
+  res.render('index', {title : 'Monthly Schedule', pageName :'pages/calendar/calendar02.ejs'})
 })
-router.get("/calender03", function(req,res,next){
-  res.render('index', {title : 'Weekly Schedule', pageName :'pages/calender/calender03.ejs'})
+router.get("/calendar03", function(req,res,next){
+  res.render('index', {title : 'Weekly Schedule', pageName :'pages/calendar/calendar03.ejs'})
 })
 module.exports = router;
